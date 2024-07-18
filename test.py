dfbd62217ae02851ebf76b1d6f585e1abc2c485d
MaxSchneider1984 <schmaximilian@gmail.com>
2024-07-18 23:32:39 +0200

    test.py

--- test.py
+++ test.py
@@ -2,22 +2,23 @@ import torch
 print(F"torch.__version__: {torch.__version__}")
 
 # Scalar
-scalar = torch.tensor(7)
-print("scalar = torch.tensor(7)")
-print(F"scalar: {scalar}")
-print(F"scalar.ndim: {scalar.ndim}")
-print(F"scalar.item(): {scalar.item()}")
+s = torch.tensor(7)
+print("===== Scalar (7) =====")
+print(F"s: {s}")
+print(F"s.ndim: {s.ndim}")
+print(F"s.item(): {s.item()}")
 
 # Vector
-vector = torch.tensor([7, 7])
-print("vector = torch.tensor([7, 7])")
-print(F"vector: {vector}")
-print(F"vector.ndim: {vector.ndim}")
-print(F"vector.shape: {vector.shape}")
+v = torch.tensor([7, 7])
+print("===== Vector ([7, 7]) =====")
+print(F"v: {v}")
+print(F"v.ndim: {v.ndim}")
+print(F"v.shape: {v.shape}")
 
 # MATRIX
-MATRIX = torch.tensor([[7, 8], [9, 10]])
-print("MATRIX = torch.tensor([[7, 8], [9, 10]])")
-print(F"MATRIX.ndim: {MATRIX.ndim}")
-print(F"MATRIX[1]: {MATRIX[1]}")
-print(F"MATRIX.shape: {MATRIX.shape}")
+M = torch.tensor([[7, 8], [9, 10]])
+print("===== Matrix ([[7, 8], [9, 10]]) =====")
+print(F"M: {M}")
+print(F"M.ndim: {M.ndim}")
+print(F"M.shape: {M.shape}")
+print(F"M[1]: {M[1]}")
